cf850cbc375004570cf5c59d511ba6c86a3c904d
nasser-sh <nasseralshawwa@yahoo.com>
2016-11-03 23:50:33 +0000

    Add Licenst to Web project files

--- Web/WebApp.cpp
+++ Web/WebApp.cpp
@@ -1,3 +1,8 @@
+/*   Copyright (c) nasser-sh 2016
+ *
+ *   Distributed under BSD-style license. See accompanying LICENSE.txt in project
+ *   directory.
+ */
 #include "WebApp.h"
 
 #include "MainWindow.h"
